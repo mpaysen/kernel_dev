ada50f7b213c3631a0670ec8a08129efb4353879
Mats Bo <mats-bo@paysen.org>
2022-11-03 10:21:58 +0100

    Comments

--- protectedmode/v4/src/idt/idt.asm
+++ protectedmode/v4/src/idt/idt.asm
@@ -5,7 +5,7 @@ idt_load:
     push ebp    ;start function
     mov ebp, esp
 
-    mov ebx, [ebp+8] ;move the value of the address ebp+8 into ebx, points to our first asm code (push ebp)
+    mov ebx, [ebp+8] ;move the value of the address ebp+8 into ebx, points to the first asm code (push ebp)
     lidt [ebx] ; load idtr_descriptor table
 
     pop ebp ;end function
